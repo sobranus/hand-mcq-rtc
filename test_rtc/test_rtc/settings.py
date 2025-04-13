725a646c7d6be22b1de342d9f85d8f5d8381546c
sobranus <raflizaki@upi.edu>
2025-04-13 22:06:47 +0700

    changes in settings.py to handle cors

--- test_rtc/test_rtc/settings.py
+++ test_rtc/test_rtc/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'corsheaders',
     'rtc',
     'channels',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,6 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://sobranus.my.id",
+]
 
 ASGI_APPLICATION = 'test_rtc.asgi.application'
 
