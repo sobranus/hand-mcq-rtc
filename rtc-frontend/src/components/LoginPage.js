430c9976ef94ffa2dfe59103a88ba9680088e381
sobranus <raflizaki@upi.edu>
2025-06-30 22:46:08 +0700

    add id and align login page

--- rtc-frontend/src/components/LoginPage.js
+++ rtc-frontend/src/components/LoginPage.js
@@ -2,26 +2,39 @@ import React, { useState } from 'react';
 
 function LoginPage({ onLogin }) {
   const [passcode, setPasscode] = useState('');
+  const [userId, setUserId] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onLogin(passcode);
+    onLogin(passcode, userId);
   };
 
   return (
     <div className="container">
       <h1>Quiz Access</h1>
       <form onSubmit={handleSubmit}>
+        <div className="user_id-section">
+          <div className="invisible-spacer"></div>
+          <strong className="form-label">ID : </strong>
+          <input 
+            type="text" 
+            className="user_id-input" 
+            placeholder="Enter ID"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+          />
+        </div>
         <div className="passcode-section">
+          <strong className="form-label">Passcode : </strong>
           <input 
             type="password" 
             className="passcode-input" 
-            placeholder="Enter passcode"
+            placeholder="Enter Passcode"
             value={passcode}
             onChange={(e) => setPasscode(e.target.value)}
           />
         </div>
-        <button type="submit" className="button">Enter Quiz</button>
+        <button type="submit" className="button">Login</button>
       </form>
     </div>
   );
