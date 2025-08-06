67d11c79220dfaf15aac9b82d41a4c86c71f3723
sobranus <raflizaki@upi.edu>
2025-08-06 22:45:10 +0700

    update instructions page

--- rtc-frontend/src/components/InstructionsPage.js
+++ rtc-frontend/src/components/InstructionsPage.js
@@ -6,17 +6,20 @@ function InstructionsPage({ onStart }) {
     <div className="container">
       <h1>Quiz Instructions</h1>
       <div className="instructions-content">
-        <video 
-          id="output-video"
-          autoPlay 
-          playsInline
-        />
+        <div className="video-area">
+          <video 
+            id="output-video"
+            autoPlay 
+            playsInline
+          />
+        </div>
         {/* Add actual instructions */}
-        <p>Please read the following instructions carefully:</p>
-        <ul>
-          <li>Use hand gestures to select answers</li>
-          <li>You have 3 minutes to complete the quiz</li>
-        </ul>
+        <p><br/><br/>Please read the following instructions carefully:<br/><br/>
+
+          Do not start the test if you dont see your camera's video in this page<br/> 
+          Use hand gestures to select answers<br/>
+          You have 30 minutes to complete the quiz<br/>
+        </p>
       </div>
       <button 
         className="button" 
