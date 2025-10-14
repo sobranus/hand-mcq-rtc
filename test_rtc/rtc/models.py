aad53ec74ccef3faa0fb9feb6eef4cee5cd16b04
sobranus <raflizaki@upi.edu>
2025-10-14 14:45:03 +0700

    add models.py for database

--- test_rtc/rtc/models.py
+++ test_rtc/rtc/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    Username = models.CharField(max_length=20)
+    Password = models.CharField(max_length=30)
+    UserExamId = models.CharField(max_length=6)
+    
+class Exams(models.Model):
+    ExamId = models.CharField(max_length=6)
+    ExamFile = models.CharField(max_length=100)
